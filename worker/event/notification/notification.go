99584800a7494af63a545174c58f5c1763c1ae5e
wachawich <guy.wpth@gmail.com>
2026-04-30 19:05:43 +0700

    chore[noti] : add noti module and noti realtime with rabbitmq

--- worker/event/notification/notification.go
+++ worker/event/notification/notification.go
@@ -9,14 +9,14 @@ import (
 func HandleNotification(event models.SocketEvent) error {
 	logger.Log("Worker: Emitting SEND_NOTIFICATION to socket server", "Notification")
 
-	chatId := event.Payload["chat_id"]
+	refId := event.Payload["ref_id"]
 	senderId := event.Payload["sender_id"]
 	body, _ := event.Payload["body"].(string)
 	title, _ := event.Payload["title"].(string)
 	receiverIdList := utils.GetStringArray(event.Payload, "target_user_sys_ids")
 
 	err := utils.EmitToSocket("SEND_NOTIFICATION", map[string]interface{}{
-		"chat_id":    chatId,
+		"ref_id":    refId,
 		"sender_id":  senderId,
 		"title":    title,
 		"body":    body,
